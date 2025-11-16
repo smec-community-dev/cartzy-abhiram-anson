1dd146f14695002d2dba3f1477f39a36fca00f02
ABHIRAM <abhiram4068@gmail.com>
2025-11-16 14:10:35 +0530

    added module for admin to add product category

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+@admin.register(Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display=('name', 'description', 'image')
+    
