e933dbd0337fa40ec9707531e759897b9192f46a
ansoncodes <ansonantony.work@gmail.com>
2025-11-17 14:42:11 +0530

    deleted unwanted templates

--- seller/urls.py
+++ seller/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("seller/register/", views.seller_register),
-    path("seller/login/", views.seller_login),
-    path("seller/dashboard/", views.seller_dashboard),
-    path("seller/addproduct/", views.add_product),
+    path("register/", views.seller_register),
+    path("login/", views.seller_login),
+    path("dashboard/", views.seller_dashboard),
+    path("addproduct/", views.add_product),
 ]
