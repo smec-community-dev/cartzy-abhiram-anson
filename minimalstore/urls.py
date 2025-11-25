9e7e313bc91267b054262f86964b4eb294894529
ansoncodes <ansonantony.work@gmail.com>
2025-11-25 17:21:07 +0530

    added core.views and templates

--- minimalstore/urls.py
+++ minimalstore/urls.py
@@ -9,7 +9,7 @@ from user.views import set_google_customer, set_google_seller, role_redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    
+    path('core/', include('core.urls')),
     path('seller/', include('seller.urls', namespace='seller')),
     path('', include('user.urls')),
 
--- seller/models.py
+++ seller/models.py
@@ -1,3 +1,4 @@
+#seller.models
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
